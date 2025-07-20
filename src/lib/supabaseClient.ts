abe946b85e8a4d759527f578031cb33a55efc24e
Leonardo Patrick <leo_viciogta@hotmail.com>
2025-07-19 23:34:32 -0300

    fix(auth): Disable Supabase auto refresh to use custom backend handler

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -3,5 +3,11 @@ import { API_CONFIG } from "../config/api.config";
 
 export const supabase = createClient(
   API_CONFIG.supabaseUrl,
-  API_CONFIG.supabaseAnonKey
+  API_CONFIG.supabaseAnonKey, {
+     auth: {
+    autoRefreshToken: false,
+    persistSession: false,
+    detectSessionInUrl: false,
+  },
+  }
 );
